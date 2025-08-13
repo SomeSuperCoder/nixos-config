a920782c757f5c4ac1946feec859faceda13f406
SomeSuperCoder <ithelper9@gmail.com>
2025-08-13 10:43:39 +0300

    Fixed tabs

--- configuration.nix
+++ configuration.nix
@@ -1,86 +1,81 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./boot.nix
-      ./zsh.nix
-    ];
+        imports = [ # Include the results of the hardware scan.
+                ./hardware-configuration.nix
+                ./boot.nix
+                ./zsh.nix
+        ];
 
-  environment.systemPackages = with pkgs; [
-    # Super important
-    home-manager
+        environment.systemPackages = with pkgs; [
+                # Super important
+                home-manager
 
-    # Graphical apps
-    firefox
-    telegram-desktop
-    ghostty
+                # Graphical apps
+                firefox
+                telegram-desktop
+                ghostty
 
-    # Regular system packages
-    wget
-    fastfetch
-    pfetch
-    tree
-    btop
-    git
-    zsh
-    fzf
-    wl-clipboard
+                # Regular system packages
+                wget
+                fastfetch
+                pfetch
+                tree
+                btop
+                git
+                zsh
+                fzf
+                wl-clipboard
     
-    # Extras
-    oh-my-zsh
-    zsh-syntax-highlighting
-    nerd-fonts.jetbrains-mono
+                # Extras
+                oh-my-zsh
+                zsh-syntax-highlighting
+                nerd-fonts.jetbrains-mono
 
-    # Programming languages
-    go
+                # Programming languages
+                go
 
-    # DPI bypass software
-    zapret
-  ];
+                # DPI bypass software
+                zapret
+        ];
 
     
-  # Enables flakes
-  nix.settings.experimental-features = ["nix-command" "flakes" ];
+        # Enables flakes
+        nix.settings.experimental-features = ["nix-command" "flakes" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+        networking.hostName = "nixos"; # Define your hostname.
+        networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
+        # Set your time zone.
+        time.timeZone = "Europe/Moscow";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+        # Enable the X11 windowing system.
+        services.xserver.enable = true;
+        services.xserver.displayManager.gdm.enable = true;
+        services.xserver.desktopManager.gnome.enable = true;
 
-  environment.variables = {
-    GSK_RENDERER = "ngl";
-  }; 
+        environment.variables = {
+                GSK_RENDERER = "ngl";
+        }; 
 
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us,ru";
-  services.xserver.xkb.options = "eurosign:e,caps:escape,grp:win_space_toggle";
+        # Define a user account. Don't forget to set a password with ‘passwd’.
+        users.users.allen = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" "input" "networkmanager" ]; # Enable ‘sudo’ for the user.
+                description = "Michael Allen";
+                shell = pkgs.zsh;
+        };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.allen = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "input" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    description = "Michael Allen";
-    shell = pkgs.zsh;
-  };
+        # DPI bypass
+        services.zapret = {
+	        enable = true;
+	        params = [
+		        "--dpi-desync=multisplit --dpi-desync-split-pos=10,midsld --dpi-desync-split-seqovl=1"
+	        ];
+        };
 
-  # DPI bypass
-  services.zapret = {
-	enable = true;
-	params = [
-		"--dpi-desync=multisplit --dpi-desync-split-pos=10,midsld --dpi-desync-split-seqovl=1"
-	];
-  };
-
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  system.stateVersion = "25.05";
+        # Most users should NEVER change this value after the initial install, for any reason,
+        # even if you've upgraded your system to a new NixOS release.
+        system.stateVersion = "25.05";
 }
 
