e58d30503aae29ae80e1d7734cb2ca765c234a4f
SomeSuperCoder <ithelper9@gmail.com>
2025-08-14 11:42:02 +0300

    GNOME desktop portal

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,7 @@
                 enable = true;
                 extraPortals = with pkgs; [
                         xdg-desktop-portal-hyprland
+                        xdg-desktop-portal-gnome
                 ];
         };
 
