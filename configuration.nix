107948cc91e910b6f8c6818195e52386551eb592
SomeSuperCoder <ithelper9@gmail.com>
2025-08-15 19:12:02 +0300

    Volume control keys and desktop portal

--- configuration.nix
+++ configuration.nix
@@ -84,7 +84,7 @@
         xdg.portal = {
                 enable = true;
                 extraPortals = with pkgs; [
-                        xdg-desktop-portal-gnome
+                        xdg-desktop-portal-hyprland
                 ];
         };
 
