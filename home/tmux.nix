35a16a5188209f215ab66be1f50f757141b2e1c8
SomeSuperCoder <ithelper9@gmail.com>
2025-08-17 18:38:01 +0300

    Themed tmux

--- home/tmux.nix
+++ home/tmux.nix
@@ -11,10 +11,16 @@
 	mouse = true;
 	keyMode = "vi";
 	clock24 = true;
-	# shortcut = "s";
 	extraConfig = ''
-	set -g prefix C-s
-	set -g status-position top
-	'';
+	    set -g status-position top
+	    
+	    set -g @catppuccin_window_status_style "rounded"
+	    set -g status-right-length 100
+	    set -g status-left-length 100
+	    set -g status-left ""
+	    set -g status-right "#{E:@catppuccin_status_application}"
+	    set -ag status-right "#{E:@catppuccin_status_session}"
+	    set -ag status-right "#{E:@catppuccin_status_uptime}"
+	    '';
     };
 }
