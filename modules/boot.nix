572dd4ae3df315ca514daabc488ab6cba48bb0a4
SomeSuperCoder <ithelper9@gmail.com>
2025-08-18 18:51:44 +0300

    Grub tweak

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,6 +4,7 @@
     boot.loader.grub.enable = true;
     boot.loader.grub.efiSupport = true;
     boot.loader.grub.device = "/dev/sda";
+    boot.loader.efi.canTouchEfiVariables = true;
     catppuccin.grub.enable = true;
     catppuccin.grub.flavor = "macchiato";
 
