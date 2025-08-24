49956bcfa1acbd159aa06760645849dd26d4b5bd
SomeSuperCoder <ithelper9@gmail.com>
2025-08-24 17:33:38 +0300

    Tmux catppuccin final version

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,24 +1,8 @@
-{ pkgs, lib, ... }:
-
 {
     catppuccin.tmux = {
 	enable = true;
 	flavor = "macchiato";
-    };
-
-    programs.tmux = {
-	enable = true;
-	mouse = true;
-	keyMode = "vi";
-	clock24 = true;
 	extraConfig = ''
-	    bind-key h select-pane -L
-	    bind-key j select-pane -D
-	    bind-key k select-pane -U
-	    bind-key l select-pane -R
-
-	    set -g status-position top
-
 	    set -g @catppuccin_window_status_style "rounded"
 	    set -g status-right-length 100
 	    set -g status-left-length 100
@@ -26,6 +10,27 @@
 	    set -g status-right "#{E:@catppuccin_status_application}"
 	    set -ag status-right "#{E:@catppuccin_status_session}"
 	    set -ag status-right "#{E:@catppuccin_status_uptime}"
+	    '';
+    };
+
+    programs.tmux = {
+	enable = true;
+	mouse = true;
+	keyMode = "vi";
+	clock24 = true;
+	disableConfirmationPrompt = true;
+	newSession = true;
+# prefix = "C-s";
+
+	extraConfig = /*conf*/ ''
+	    bind-key h select-pane -L
+	    bind-key j select-pane -D
+	    bind-key k select-pane -U
+	    bind-key l select-pane -R
+
+	    bind-key r source ~/.config/tmux/tmux.conf
+
+	    set -g status-position top
 	    '';
     };
 }
