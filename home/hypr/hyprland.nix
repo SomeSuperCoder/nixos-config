af9a7b9030d505e2734d176a8c39e711a68d0c18
SomeSuperCoder <ithelper9@gmail.com>
2025-08-25 09:42:09 +0300

    Pavucontrol windowrule

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -218,6 +218,12 @@
 			windowrule = [
 			    "suppressevent maximize, class:.*"
 				"nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+				# Pavucontrol
+				"float,title:(Volume Control)"
+				"center,title:(Volume Control)"
+				"opacity 0.7,title:(Volume Control)"
+				"pin,title:(Volume Control)"
+				"minsize 800 300,title:(Volume Control)"
 			];
 		};
 	};
