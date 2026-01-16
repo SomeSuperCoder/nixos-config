68d05b852eb6d2a4eec7206c640549bbdf35a7d2
SomeSuperCoder <ithelper9@gmail.com>
2026-01-16 20:08:48 +0300

    Made kitty and tmux look perfect

--- home/kitty.nix
+++ home/kitty.nix
@@ -7,11 +7,11 @@
   programs.kitty = {
     enable = true;
     settings = {
-      # background_opacity = 0.5;
+      background_opacity = 0.5;
       confirm_os_window_close = 0;
       font_family = "JetBrains Mono Nerd Font";
       font_size = 13;
-      window_padding_width = 4;
+      # window_padding_width = 4;
     };
   };
 }
--- home/tmux.nix
+++ home/tmux.nix
@@ -1,36 +1,55 @@
 {
-    catppuccin.tmux = {
-	enable = true;
-	flavor = "macchiato";
-	extraConfig = ''
-	    set -g @catppuccin_window_status_style "rounded"
-	    set -g status-right-length 100
-	    set -g status-left-length 100
-	    set -g status-left ""
-	    set -g status-right "#{E:@catppuccin_status_application}"
-	    set -ag status-right "#{E:@catppuccin_status_session}"
-	    set -ag status-right "#{E:@catppuccin_status_uptime}"
-	    '';
-    };
+  catppuccin.tmux = {
+    enable = true;
+    flavor = "macchiato";
+    extraConfig = ''
+      	    set -g @catppuccin_window_status_style "rounded"
+      	    set -g status-right-length 100
+      	    set -g status-left-length 100
+      	    set -g status-left ""
+      	    set -g status-right "#{E:@catppuccin_status_application}"
+      	    set -ag status-right "#{E:@catppuccin_status_session}"
+      	    set -ag status-right "#{E:@catppuccin_status_uptime}"
 
-    programs.tmux = {
-	enable = true;
-	mouse = true;
-	keyMode = "vi";
-	clock24 = true;
-	disableConfirmationPrompt = true;
-	newSession = true;
-# prefix = "C-s";
+      	# Set status bar to use terminal's background
+      set-option -g status-style bg=default,fg=white
 
-	extraConfig = /*conf*/ ''
-	    bind-key h select-pane -L
-	    bind-key j select-pane -D
-	    bind-key k select-pane -U
-	    bind-key l select-pane -R
+      # Set window status to use terminal's background
+      set-window-option -g window-status-style bg=default
+      set-window-option -g window-status-current-style bg=default
 
-	    bind-key r source ~/.config/tmux/tmux.conf
+      # Set message and command prompt background to default
+      set-option -g message-style bg=default,fg=white
+      set-option -g message-command-style bg=default,fg=white
 
-	    set -g status-position top
-	    '';
-    };
+      # Make pane borders transparent (or match terminal)
+      set-option -g pane-border-style fg=default
+      set-option -g pane-active-border-style fg=default
+    '';
+  };
+
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    keyMode = "vi";
+    clock24 = true;
+    disableConfirmationPrompt = true;
+    newSession = true;
+    # prefix = "C-s";
+
+    extraConfig =
+      /*
+      conf
+      */
+      ''
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+
+        bind-key r source ~/.config/tmux/tmux.conf
+
+        set -g status-position top
+      '';
+  };
 }
